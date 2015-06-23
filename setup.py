324d9617016e4dc8846a7f187948bb9fad9cd541
tetsuwo <tetsuwo.oishi@gmail.com>
2015-06-23 23:39:22 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages
+from setuptools import setup
+import sys
+
+sys.path.append('./src')
+sys.path.append('./tests')
+
+setup(
+    name='vimman-api',
+    version='0.1',
+    description='This is test codes for travis ci',
+    packages=find_packages(),
+    test_suite='run.suite'
+)
