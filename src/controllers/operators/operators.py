f3aa705371d907dc88e15eb70abb7de8c900984c
tetsuwo <tetsuwo.oishi@gmail.com>
2015-06-24 22:44:15 +0900

    Remove unused imported

--- src/controllers/operators/operators.py
+++ src/controllers/operators/operators.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
 from flask import jsonify
-from flask import redirect
 from flask import request
-from flask import session
 from helpers.crossdomain import crossdomain
 from models.model import *  # NOQA
 from datetime import datetime as dt
